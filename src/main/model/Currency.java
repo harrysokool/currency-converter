83b73a5b475e3ece609ebe307ed7ed945d9fd237
“harrysokool” <harry1232003@icloud.com>
2022-10-10 13:06:09 -0700

    specified classes in model

--- src/main/model/Currency.java
+++ src/main/model/Currency.java
@@ -2,5 +2,92 @@ package model;
 // Represent the name of a currency and the rate to USD of the currency.
 
 public class Currency {
+    //fields
+    private String currencyName; // name of the currency
+    private double rateToOneUSD; // rate of the currency to 1 USD. e.g.) 1 USD = rate * 8 HKD
+    private double amount; // amount of the currency
+    private double usd; // amount of the currency to usd
+
+    //methods
+    /*
+     * REQUIRES: currencyName has a non-zero length, rateToOneUSD must be >= 0.
+     * EFFECTS: name of currency is set to currencyName; rate to 1 USD to set to
+     *          rateToOneUSD; amount of currency set to 0; set usd of this currency
+     *          to 0.
+     */
+    public Currency(String currencyName, double rateToOneUSD) {
+        setCurrencyName(currencyName);
+        updateRate(rateToOneUSD);
+        amount = 0;
+        usd = 0;
+    }
+
+    public double getUsd() {
+        return usd;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getCurrencyName() {
+        return currencyName;
+    }
+
+    public double getRateToOneUSD() {
+        return rateToOneUSD;
+    }
+
+    public void setCurrencyName(String currencyName) {
+        this.currencyName = currencyName;
+    }
+
+    public void setUsd(double usd) {
+        this.usd = usd;
+    }
+
+    /*
+     * REQUIRES: amount >= 0
+     * MODIFIES: this
+     * EFFECTS: amount is updated
+     */
+    public void amountToConvert(double amount) {
+        this.amount = amount;
+    }
+
+    /*
+     * REQUIRES: rateToOneUSD >= 0
+     * MODIFIES: this
+     * EFFECTS: rate to 1 USD of the currency is updated
+     */
+    public void updateRate(double rateToOneUSD) {
+        this.rateToOneUSD = rateToOneUSD;
+    }
+
+    /*
+     * REQUIRES: amount >= 0
+     * MODIFIES: this
+     * EFFECTS: usd of the currency is updated
+     */
+    public void toUSD(double amount) {
+        amountToConvert(amount);
+        setUsd(getAmount() * getRateToOneUSD());
+    }
+
+    /*
+     * EFFECTS: returns a string representation of currency
+     */
+    @Override
+    public String toString() {
+        return "Currency{"
+                + "Currency name='" + currencyName + '\''
+                + ", Rate to ene USD=" + rateToOneUSD
+                + '}';
+    }
 
 }
+
+
+
+
+
--- src/main/model/CurrencyList.java
+++ src/main/model/CurrencyList.java
@@ -1,5 +1,57 @@
 package model;
 // A list of currency, and can be added or removed.
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CurrencyList {
+    // fields
+    private final List<Currency> currencies; // list of currencies
+
+    //methods
+    /*EFFECTS: initialize an empty list of currencies
+     */
+    public CurrencyList() {
+        currencies = new ArrayList<>();
+    }
+
+    /*
+     * REQUIRES: a valid currency
+     * MODIFIES: this
+     * EFFECTS: add currency into currencies.
+     */
+    public String addCurrency(Currency currency) {
+        currencies.add(currency);
+        return currency.getCurrencyName();
+    }
+
+    /*
+     * REQUIRES: a valid currency
+     * MODIFIES: this
+     * EFFECTS: remove currency from currencies.
+     */
+    public String removeCurrency(Currency currency) {
+        currencies.remove(currency);
+        return currency.getCurrencyName();
+    }
+
+    /*
+     * EFFECTS: list currencies
+     */
+    public List<Currency> listCurrencies() {
+        return currencies;
+    }
+
+    /*
+     * REQUIRES: amount >= 0
+     * MODIFIES: this
+     * EFFECTS: rate to 1 USD of the currency is updated
+     */
+    public double convertCurrency(Currency prevCurrency, Currency nextCurrency, double amount) {
+        double finalAmount;
+        prevCurrency.toUSD(amount);
+        finalAmount = prevCurrency.getUsd() * nextCurrency.getRateToOneUSD();
+        return finalAmount;
+    }
+
 }
