338642ea1b24e376ee1a877ca4f65ae12a43f45b
“harrysokool” <harry1232003@icloud.com>
2022-11-22 19:40:57 -0800

    Phase 4: Task 3

--- src/main/model/Event.java
+++ src/main/model/Event.java
@@ -4,7 +4,7 @@ import java.util.Calendar;
 import java.util.Date;
 
 
-/**
+/*
  * Represents an alarm system event.
  */
 public class Event {
@@ -12,27 +12,27 @@ public class Event {
     private Date dateLogged;
     private String description;
 
-    /**
-     * Creates an event with the given description
+    /*
+     * EFFECTS: Creates an event with the given description
      * and the current date/time stamp.
-     * @param description  a description of the event
+     * @param description a description of the event
      */
     public Event(String description) {
         dateLogged = Calendar.getInstance().getTime();
         this.description = description;
     }
 
-    /**
-     * Gets the date of this event (includes time).
+    /*
+     * EFFECTS: Gets the date of this event (includes time).
      * @return  the date of the event
      */
     public Date getDate() {
         return dateLogged;
     }
 
-    /**
-     * Gets the description of this event.
-     * @return  the description of the event
+    /*
+     * EFFECTS: Gets the description of this event.
+     * @return the description of the event
      */
     public String getDescription() {
         return description;
